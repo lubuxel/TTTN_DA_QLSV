7ce462b7f29aca29faeb1861d021813af68d8dea
lubuxel <trancongtruc011812@gmail.com>
2023-10-05 18:59:37 +0700

    QLSV

--- DataBace/src/route/web.js
+++ DataBace/src/route/web.js
@@ -1,11 +1,10 @@
 import express from "express";
+import homeController from "../controllers/homeController";
 
 let router = express.Router();
 
 let initWebRouter = (app) => {
-    router.get('/', (req, res) => {
-        return res.send('hello')
-    });
+    router.get('/', homeController.getHomePage);
     return app.use("/", router);
 }
 module.exports = initWebRouter;
