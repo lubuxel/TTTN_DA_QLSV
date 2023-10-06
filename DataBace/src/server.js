0d9783d0c58b35b0c5ce0f43cef003b364b554b0
lubuxel <trancongtruc011812@gmail.com>
2023-10-06 15:59:37 +0700

    QLSV

--- DataBace/src/server.js
+++ DataBace/src/server.js
@@ -2,6 +2,7 @@ import express from "express";
 import bodyParser from "body-parser";
 import viewEngine from "./config/viewEngine";
 import initWebRouter from "./route/web";
+import connectDB from "./config/connectDB";
 require('dotenv').config();
 
 let app = express();
@@ -12,7 +13,11 @@ app.use(bodyParser.urlencoded({ extended: true }));
 viewEngine(app);
 initWebRouter(app);
 
+connectDB();
+
+
 let PORT = process.env.PORT || 1812;
 app.listen(PORT, () => {
     console.log("Check" + PORT)
+
 })
